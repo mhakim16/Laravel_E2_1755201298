e309b261f80a13174754699a7fb0fa24bb735493
mhakim16 <muhammad.auliahakimuddin@gmail.com>
2020-08-13 21:08:13 +0700

    Pertemuan 12

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
 	<title>@yield('title')</title>
 	<script src="{{ asset('js/app.js') }}"></script>
 	<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+	<link rel="stylesheet" href="//cdn.datatables.net/1.10.7/css/jquery.dataTables.min.css">
+	<script src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
 </head>
 <body>
 	@include('layouts.navbar')
--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -5,5 +5,34 @@
 @section('bread2', 'Daftar')
 
 @section('content')
-    <p> This is my body Content.</p>
+    <h1>Master Mahasiswa</h1>
+	<table class="table table-striped " id="mhs-table" >
+		<thead>
+			<tr>
+				<th>No</th>
+				<th>NIM</th>
+				<th>Nama Lengkap</th>
+				<th>Program Studi</th>
+				<th>Alamat</th>
+			</tr>
+		</thead>
+
+
+	</table>
+	<script >
+$(function() {
+	$('#mhs-table').DataTable({
+		processing: true,
+		serverSide: true,
+		ajax: "{{ route('mhs_list')}}",
+		columns: [
+			{ data: 'DT_RowIndex', name: 'DT_RowIndex'},
+			{ data: 'nim', name: 'nim'},
+			{ data: 'nama_lengkap', name: 'nama_lengkap'},
+			{ data: 'prodi', name: 'prodi'},
+			{ data: 'alamat', name: 'alamat'},
+		]
+	});	
+});
+</script>
 @endsection
